6c89f749522a0497c5fbc2044d3a32af5c6551e7
yuka383 <gongyouhua981@gmail.com>
2025-10-30 21:47:06 +0900

    Todo に関するファイル

---
+++ app/app/Models/TodoItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TodoItem extends Model
+{
+    use HasFactory;
+     protected $fillable = ['user_id','title','is_done'];
+}
